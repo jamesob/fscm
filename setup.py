56dbbc63e72696e8b92ec9cce432f7f00ebdf0b1
James O'Beirne <james.obeirne@pm.me>
2022-02-03 15:25:33 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,53 +1,28 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-"""The setup script."""
-
-from setuptools import setup, find_packages
-
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = []
-
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest>=3', ]
 
 setup(
     author="James O'Beirne",
     author_email='james.obeirne@pm.me',
-    python_requires='>=3.5',
+    python_requires='>=3.9',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
-    description="Fucking simple configuration management",
-    entry_points={
-        'console_scripts': [
-            'fscm=fscm.cli:main',
-        ],
-    },
-    install_requires=requirements,
+    description="fucking simple configuration management",
     license="MIT license",
-    long_description=readme + '\n\n' + history,
     include_package_data=True,
+    long_description=readme,
+    long_description_content_type='text/markdown',
     keywords='fscm',
     name='fscm',
-    packages=find_packages(include=['fscm', 'fscm.*']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
+    py_modules=['fscm'],
     url='https://github.com/jamesob/fscm',
-    version='0.1.0',
-    zip_safe=False,
+    version='0.0.1',
 )
